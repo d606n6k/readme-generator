50d200b3c46a3d12f86430bfec9a1fba5b37ab6d
William Lucht <aaronlucht@gmail.com>
2021-04-09 16:15:02 -0700

    can generate file with template literals. Still need to debug createReadMe function

--- example-readme.md
+++ example-readme.md
@@ -1,11 +1,7 @@
 # readme-generator
 
 ## Description
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+`${questions.projectDescription}`
 ## Table of Contents (Optional)
 - [Installation](#installation)
 - [Usage](#usage)
@@ -17,23 +13,24 @@ Run `npm i`.
 
 ## Usage
 
-Run `node index.js` in the terminal.
+Run `node index.js` in the terminal to be prompted with questions for your readme file!
+
+Here is a quick video guide: *video link*
 
 ## Credits
 
 Created by [d606n6k](https://github.com/d606n6k)
 
 ## License
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
----
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+License: `${question.license}`
+
 ## Badges
 ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
 Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
 ## Features
 If your project has a lot of features, list them here.
 ## How to Contribute
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+If you would like to contribute to this project, please fork the project as needed.
 ## Tests
 To run tests, run the following command:
 `npm test`
--- index.js
+++ index.js
@@ -3,33 +3,43 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [{
+const questions = [
     /* Pass your questions in here */
-    type: 'input',
-    message: 'Please describe your project.',
-    name: 'projectDescription',
-  },
-  {
-    type: 'input',
-    message: 'Where are you located?',
-    name: 'location',  
-},
-{
-    type: 'input',
-    message: 'Tell us about yourself',
-    name: "bio",
-},
-{
-    type: 'input',
-    message: 'What is your LinkedIn URL?',
-    name: 'linkedin',
-},
-{
-    type: 'input',
-    message: 'What is your Github username?',
-    name: 'github',
-},];
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'projName',
+    },
+    {
+        type: 'input',
+        message: 'Please describe your project.',
+        name: 'projectDescription',
+    },
+    {
+        type: 'checkbox',
+        message: 'Enter a usage license:',
+        choices:["MIT", "BSD3", "none"], 
+        name: 'license', 
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: "githubUsername",
+    },
+    {
+        type: 'input',
+        message: 'What is the email address contributors/users should contact you at?',
+        name: 'email',
+    },
 
+];
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+// prompt user
 inquirer
   .prompt(questions)
   .then((data) => {
@@ -40,22 +50,49 @@ inquirer
     );
   });
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-function createReadMe(){
-    const readMeStuff =  `README MARK DOWN HERE`;
-    return readMeStuff;
-  };
-
-// TODO: Create a function to initialize app
-function init() {
-// prompt user
-
 // then call generateMarkdown
 
 // then write the file
+function createReadMe(questions){
+    const readMeStuff =  `# readme-generator
 
+## Description
+${data.questions.projectDescription}
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+    
+Run \`npm i\`.
+    
+## Usage
+    
+Run \`node index.js\` in the terminal to be prompted with questions for your readme file!
+    
+Here is a quick video guide: *video link*
+    
+## Credits
+    
+Created by [${data.question.githubUsername}](https://github.com/${data.question.githubUsername})
+    
+## License
+License: *need license badge template literal here*
+    
+## Badges
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+## Features
+If your project has a lot of features, list them here.
+## How to Contribute
+If you would like to contribute to this project, please fork the project as needed.
+## Tests
+To run tests, run the following command:
+\`npm test\``
+    return readMeStuff;
+  };
 
 }
 
