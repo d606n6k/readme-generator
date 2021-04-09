633eb754bf0c3ce4475b7e2386a34b210a630307
William Lucht <aaronlucht@gmail.com>
2021-04-09 15:10:39 -0700

    started work on the prompt questions

--- index.js
+++ index.js
@@ -3,7 +3,37 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    /* Pass your questions in here */
+    type: 'input',
+    message: 'What is your name?',
+    name: 'username',
+  },
+  {
+    type: 'input',
+    message: 'Where are you located?',
+    name: 'location',  
+},
+{
+    type: 'input',
+    message: 'Tell us about yourself',
+    name: "bio",
+},
+{
+    type: 'input',
+    message: 'What is your LinkedIn URL?',
+    name: 'linkedin',
+},
+{
+    type: 'input',
+    message: 'What is your Github URL?',
+    name: 'github',
+},];
+
+inquirer
+  .prompt(questions)
+  .then()
+  .then();
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
